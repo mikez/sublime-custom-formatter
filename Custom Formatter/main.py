b7dae3e6c483cbfd74a2229873923201fdcbacf4
Michael B <consulting@belfrage.net>
2019-10-05 11:53:31 +0200

    initial commit

---
+++ Custom Formatter/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+"""
+Sublime Custom Formatter
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Format code upon save with a custom shell command.
+
+Configure in the syntax-specific settings under the key "formatter".
+
+Example for `JavaScript.sublime-settings`:
+
+    {
+        "formatter": [
+          "/usr/local/bin/prettier", "--write", "$1.js"
+        ]
+    }
+
+Note: `$1.extension` is replaced with a temporary file containing the
+current code to format.
+"""
+
+import logging
+import os
+import re
+from subprocess import Popen, PIPE
+import tempfile
+
+import sublime
+import sublime_plugin
+
+
+logging.basicConfig(
+    level=logging.INFO, format=" %(asctime)s - %(levelname)s - %(message)s"
+)
+
+FILENAME_PATTERN = re.compile(r"\$1(\.\w+)$", flags=re.ASCII)
+
+
+class RunFormatEventListener(sublime_plugin.EventListener):
+    @classmethod
+    def on_pre_save(cls, view):
+        # logging.info('Hello Pre Save Async')
+        view.run_command("run_custom_formatter")
+
+    @classmethod
+    def on_post_save_async(cls, view):
+        # logging.info('Hello Post Save Async')
+        pass
+
+
+class RunCustomFormatterCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        formatter = view.settings().get("formatter")
+        if formatter:
+            region = sublime.Region(0, view.size())
+            text = view.substr(region)
+
+            # logging.info("Formatting with: " + formatter[0])
+            text = format_text(text, formatter)
+
+            view.replace(edit, region, text)
+
+
+def format_text(text, command):
+    suffix = extract_extension(command)
+    try:
+        with tempfile.NamedTemporaryFile(
+            mode="w+", delete=False, suffix=suffix
+        ) as file:
+            file.write(text)
+            file.close()
+            command = [
+                file.name if FILENAME_PATTERN.match(item) else item for item in command
+            ]
+            run_shell_command(command)
+    finally:
+        if os.path.isfile(file.name):
+            with open(file.name) as file2:
+                result = file2.read()
+            os.remove(file.name)
+
+    return result
+
+
+def extract_extension(command):
+    for item in command:
+        match = FILENAME_PATTERN.match(item)
+        if match:
+            return match.group(1)
+    return ""
+
+
+def run_shell_command(command):
+    process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode > 0:
+        logging.info(command)
+        logging.error(stderr)
---
+++ README.md
@@ -0,0 +1,24 @@
+# Sublime Custom Code Formatter
+
+Sublime Package. Format code upon save with a custom shell command.
+
+## Usage
+
+1. Open the Sublime Syntax-specific settings for a particular language. For example, `JavaScript.sublime-settings`.
+2. Add the `formatter` key with the words of the command to run. For example:
+```
+    "formatter": [
+        "/usr/local/bin/prettier",
+        "--config", "/configs/prettier.config.json",
+        "--write",
+        "$1.js",
+    ]
+```
+
+Note: Any `$1.extension` item in the command list is replaced by a temporary file used to format the current code.
+
+## Installation
+
+Copy the `Custom Formatter` directory into your Sublime Packages folder. For example, on macOS this is `~/Library/Application Support/Sublime Text 3/Packages/`.
+
+Enjoy! ✨
